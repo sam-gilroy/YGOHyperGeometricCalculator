410edd79b303af2cacc6462cde559b763074bcea
sam-gilroy <sam.gilroy2@gmail.com>
2024-02-18 11:39:36 -0600

    initial form template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormBuilder, Validators, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'untitled';
+export class AppComponent implements OnInit{
+  fg: FormGroup;
+  cardtypes: String[];
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){
+    this.fg = new FormGroup({});
+    this.cardtypes = ['Starter',
+      'Extender',
+      'Utility',
+      'Hand Trap',
+      'Engine Requirement',
+      'Non Engine']
+  }
+
+  ngOnInit(): void {
+  this.fg = this.formBuilder.group({});
+  }
+
+  onSubmit(e: Event) {
+
+  }
+
 }
