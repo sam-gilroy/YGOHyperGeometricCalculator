3df0f3b7153d5c6a24423104cf795dc3cfcb8379
sam-gilroy <sam.gilroy2@gmail.com>
2024-02-18 18:32:08 -0600

    First version of geometric calculations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormBuilder, Validators, FormGroup, ReactiveFormsModule, FormArray} from '@angular/forms';
 import {NgForOf} from "@angular/common";
+// @ts-ignore
+import jStat from "jstat";
+
 
 @Component({
   selector: 'app-root',
@@ -22,6 +25,7 @@ export class AppComponent implements OnInit{
   engine_req_count: number;
   nonengine_count: number;
   engine_count: number;
+  starterResults: number[][];
 
   constructor(
     private formBuilder: FormBuilder
@@ -37,6 +41,7 @@ export class AppComponent implements OnInit{
     this.engine_req_count = 0;
     this.nonengine_count = 0;
     this.engine_count = 0;
+    this.starterResults = [];
 
     const decklist = [];
     decklist.push(this.formBuilder.group({
@@ -61,8 +66,15 @@ export class AppComponent implements OnInit{
   }
 
   onSubmit(e: Event) {
+    this.clearResults();
     this.prepForAnalysis();
-    console.log(this.actualdecklist);
+    console.log(this.PerformHGD(1, this.starter_count, 5, this.actualdecklist.length));
+    this.starterResults.push([1,Number(this.PerformHGD(1, this.starter_count, 5, this.actualdecklist.length))]);
+    this.starterResults.push([2,Number(this.PerformHGD(2, this.starter_count, 5, this.actualdecklist.length))]);
+    this.starterResults.push([3,Number(this.PerformHGD(3, this.starter_count, 5, this.actualdecklist.length))]);
+    this.starterResults.push([4,Number(this.PerformHGD(4, this.starter_count, 5, this.actualdecklist.length))]);
+    this.starterResults.push([5,Number(this.PerformHGD(5, this.starter_count, 5, this.actualdecklist.length))]);
+    console.log();
   }
 
   AddNewRow() {
@@ -128,7 +140,19 @@ export class AppComponent implements OnInit{
   }
 
   PerformHGD(required_amount_of_successes:number,possible_successes:number,chances_at_success:number,total_values:number){
+    return ((jStat.hypgeom.pdf(required_amount_of_successes,total_values,possible_successes,chances_at_success))*100).toFixed(2);
+  }
 
+  clearResults(){
+    this.actualdecklist = [];
+    this.starter_count = 0;
+    this.extender_count = 0;
+    this.board_breaker_count = 0;
+    this.handtrap_count = 0;
+    this.engine_req_count = 0;
+    this.nonengine_count = 0;
+    this.engine_count = 0;
+    this.starterResults = [];
   }
 }
 
